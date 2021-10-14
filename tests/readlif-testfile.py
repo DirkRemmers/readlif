bf800ced89d0c5ddcbb4f0f0afc05623f5267f37
Dirk Remmers <DirkRemmers@outlook.com>
2021-10-14 18:00:21 +0200

    Update readlif-testfile.py

--- tests/readlif-testfile.py
+++ tests/readlif-testfile.py
@@ -5,7 +5,7 @@ from readlif.reader import LifFile
 import numpy as np
 import napari
 
-path = "./tests/navi_mosaic.lif"
+path = "./test-files/Exp191.lif"
 raw_data = LifFile(path)
 img_list = [i for i in raw_data.get_iter_image()]
 print(len(img_list))
@@ -15,7 +15,7 @@ z_nr_list = list(range(0,len([i for i in raw_image.get_iter_z()])))
 c_nr_list = list(range(0,len([i for i in raw_image.get_iter_c()])))
 m_nr_list = list(range(0,len([i for i in raw_image.get_iter_m()])))
 
-m_nr = 7
+m_nr = 0
 
 for z_nr in z_nr_list:
     if z_nr == 0:
@@ -24,9 +24,8 @@ for z_nr in z_nr_list:
     else:
         layers_c0 = np.dstack((layers_c0, np.asarray(raw_image.get_frame(z = z_nr, t = 0, c = 0, m = m_nr))))
         layers_c1 = np.dstack((layers_c1, np.asarray(raw_image.get_frame(z = z_nr, t = 0, c = 1, m = m_nr))))
-    
-with napari.gui_qt():
-    viewer = napari.Viewer()
-    viewer.add_image(layers_c0, scale = (1,1,14),colormap='blue', blending='additive')
-    viewer.add_image(layers_c1, scale = (1,1,14),colormap='yellow', blending='additive')
-    viewer.dims.ndisplay = 3
+
+viewer = napari.Viewer()
+viewer.add_image(layers_c0, scale = (1,1,14),colormap='blue', blending='additive')
+viewer.add_image(layers_c1, scale = (1,1,14),colormap='yellow', blending='additive')
+viewer.dims.ndisplay = 3
